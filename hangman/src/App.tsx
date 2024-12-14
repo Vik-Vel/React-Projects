ce98e733d98fa505398b3a544cc7f1b0e6a6b522
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-12-14 20:17:50 +0200

    Starting to bild the app

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import words from "./wordList.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]; //This chooses a random index from 0 to the length of words (not including it), then returns the element at that index.
+  });
+
+  //Array of strings
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        maxWidth: "800px", //Limits the width to 800 pixels.
+        display: "flex", //Makes the container flexbox.
+        flexDirection: "column", // Arranges the elements one below the other.
+        gap: "2rem", //2 rem spacing between elements.
+        margin: "0 auto", // Centers the container in the middle of the screen.
+        alignItems: "center", //Aligns the elements in the center.
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
