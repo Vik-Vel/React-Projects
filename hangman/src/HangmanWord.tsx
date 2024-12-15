a4c36b8fb7fc77e4f161995a69a4888c3b18e3c6
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-12-15 14:48:23 +0200

    Create logic about handmanWord

--- hangman/src/HangmanWord.tsx
+++ hangman/src/HangmanWord.tsx
@@ -1,1 +1,28 @@
-export function HangmanWord() {}
+export function HangmanWord() {
+  const word = "test";
+  const guessedLetter = ["t", "e", "g"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetter.includes(letter) ? "visible" : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
